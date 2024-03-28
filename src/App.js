2e41090eab61f6b9d53c7e59046f86148507cb8b
Spinach_M33 <spinach_m33@Spinach-M33s-MacBook-Pro.local>
2024-03-28 16:10:33 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          helloo!!
+          Welcome Home.
         </p>
         <a
           className="App-link"
